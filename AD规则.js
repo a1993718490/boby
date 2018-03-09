816b20feb7531c98e1ec2ba0578d202036fa2fcf
a1993718490 <1993718490@qq.com>
2018-03-10 01:11:48 +0800

    规则

---
+++ AD规则.js
@@ -0,0 +1,7 @@
+
+
+|http://www.iqiyi.com/v_$s@</head>@<script src="https://coding.net/u/b1993718490/p/MP4/git/raw/master/AD6666.js"></script></head>@
+
+|http://www.iqiyi.com/v_$s@</body>@<script>insVIP('mod-play-tit','flashbox');</script></body>@
+
+|http://www.iqiyi.com/v_##div[style*="absolute"][style*="visibility"]
